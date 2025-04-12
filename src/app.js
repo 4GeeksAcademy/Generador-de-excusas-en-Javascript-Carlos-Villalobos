1e1db9f2ff49f7a64e3d3c3dd1a9cc21cc2ca8cf
Dixthor <carlosvillalobos9704@gmail.com>
2025-04-12 04:07:09 +0000

    Mejore el codigo reduciendo la cantidad de variables para el Math random y también le puse const porque siempre hará lo mismo, para la frase lo hice con backticks para no añadir tantas comillas

--- src/app.js
+++ src/app.js
@@ -6,17 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  const excusa = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-  let quien = Math.floor(Math.random() * who.length);
-  let accion = Math.floor(Math.random() * action.length);
-  let que = Math.floor(Math.random() * what.length);
-  let cuando = Math.floor(Math.random() * when.length);
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let frase = who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando];
-  let parrafo = document.getElementById('excuse');
-  parrafo.innerHTML = frase;
+  const frase = `${excusa(who)} ${excusa(action)} ${excusa(what)} ${excusa(when)}`;
+  document.getElementById('excuse').innerHTML = frase;
 };
